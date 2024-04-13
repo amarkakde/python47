9c3fb2b024554d9aaa7d6462fb51165d1812146b
AmarKakde <amarkakde21@gmail.com>
2024-04-13 14:49:08 +0530

    sum of all integers in list

---
+++ List.py
@@ -0,0 +1,13 @@
+# List Exercises
+
+# 1.Write a Python program to sum all the items in a list.
+
+def sum_of_all_elements_in_the_list(List: list[int]) -> int:
+    if isinstance(List, list) and all(isinstance(item, int) for item in List):
+        sum = 0
+
+        for item in List:
+            sum += item
+        return sum
+    else:
+        raise ValueError('Input must be list of integer')
