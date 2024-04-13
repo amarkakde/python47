8f49f49f7ff50fbe07e16c35c61fa4c88e7c7d97
AmarKakde <amarkakde21@gmail.com>
2024-04-13 16:11:53 +0530

    merge df_area and df_population

--- pandas_practice.py
+++ pandas_practice.py
@@ -11,6 +11,11 @@ def world_population():
     df_country_area = df_country_area[1].iloc[:, 1:6]
 
     df_country_population = pd.read_html(country_and_population) 
-    df_country_population = df_country_population.iloc[:, 1:6]
+    df_country_population = df_country_population[0].iloc[:, 1:6]
+
+    df_country_population.loc[0, 'Location'] = 'Earth'
+
+    df = df_country_area.merge(df_country_population, left_on='Country / dependency', right_on='Location', how='outer')
+    df['Country / dependency'] = df['Country / dependency'].fillna(df['Location'])
 
 
