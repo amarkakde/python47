8e30a3211b4e4b827eaeda33682ec82597d499bf
AmarKakde <amarkakde21@gmail.com>
2024-04-13 16:02:13 +0530

    Dataframe Country by Area

--- pandas_practice.py
+++ pandas_practice.py
@@ -1,1 +1,13 @@
 import pandas as pd
+
+# create a dataframe from wikipedia 
+
+def world_population():
+    country_and_area = 'https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_area'
+    country_and_population = 'https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population'
+    country_and_population_density = 'https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population_density'
+
+    df_country_area = pd.read_html(country_and_area)
+    df_country_area = df_country_area[1].iloc[:, 1:]
+    df_country_area = df_country_area.loc[:, 'Country / dependency':'% water']
+    
