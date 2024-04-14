7c371cea04aaa3af907fb5bdf34f2ad33e37ec54
AmarKakde <amarkakde21@gmail.com>
2024-04-15 02:39:18 +0530

    capitalize every letter in string

--- strings.py
+++ strings.py
@@ -2,7 +2,7 @@
  Write a Python program to count the number of characters (character frequency) in a string.
  """
 
-def character_frequency_in_string(string):
+def character_frequency_in_string(string) -> dict:
 	char_frequency_dict = {}
 
 	for char in string:
@@ -12,3 +12,13 @@ def character_frequency_in_string(string):
 			char_frequency_dict[char] = 1
 
 	return char_frequency_dict
+
+"""
+Write a Python program to capitalize the first letter of each word in a given string.
+"""
+
+def capitalize_first_letter_of_each_string(string) -> str:
+	words = [word.capitalize() for word in string.split()]
+
+	return ' '.join(words)
+
