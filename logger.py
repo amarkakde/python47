b7067ccae141e4b7d27ab068f874ae6ea44de024
AmarKakde <amarkakde21@gmail.com>
2024-04-15 11:04:13 +0530

    name instead of root in logs

--- logger.py
+++ logger.py
@@ -1,5 +1,7 @@
 import logging
 
+logging.basicConfig(level=logging.DEBUG, filename='logger.txt')
+
 def logging_initial():
     logger = {
         'CRITICAL':logging.CRITICAL,
@@ -8,9 +10,17 @@ def logging_initial():
         'INFO':logging.INFO,
         'DEBUG': logging.DEBUG}
 
-    logging.basicConfig(level=logging.DEBUG)
     logging.critical('critial message')
     logging.warning('warning message')
     logging.error('error message')
     logging.info('info messaege')
     logging.debug('debug message')
+
+def try_logger_first_time():
+    logger = logging.getLogger(try_logger_first_time.__name__)
+    logger.info(f'in {try_logger_first_time.__name__} function started.')
+    for value in range(10):
+        logger.log(logging.INFO, msg=f'{value} calculated')
+    logger.info('{try_logger_first_time.__name__} function completed')
+
+try_logger_first_time()
