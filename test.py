9a92af044d87d703f23370c5f9d6dff9ab7aff9f
AmarKakde <amarkakde21@gmail.com>
2024-04-16 10:41:45 +0530

    test.py for root logging

---
+++ test.py
@@ -0,0 +1,6 @@
+import logging
+from pathlib import Path
+import os
+
+logging.basicConfig(level=logging.INFO, filename='test.log.txt', 
+                    format='%(levelname)s:%(asctime)s:%(name)s:%(message)s')
