942f10cc3fb4138f409a849646424066b4718ad1
AmarKakde <amarkakde21@gmail.com>
2024-04-23 12:34:38 +0530

    2nd Q

--- List2.py
+++ List2.py
@@ -14,4 +14,19 @@ def remove_duplicates_sorted(lst: list[int]) -> list[int]:
 	return lst
 
 
+'''
+Write a Python function called rotate_matrix_clockwise that takes a 
+square matrix (list of lists) as input and rotates the matrix 90 degrees clockwise. 
+The function should modify the input matrix in-place and return the rotated matrix.
+'''
 
+def rotate_matrix_clockwise(lst: list[list]) -> list[list]:
+	matrix = []
+	for i in range(len(lst)):
+		temp = []
+		for j in range(len(lst[0])):
+			temp.append(lst[j][i])
+		temp.reverse()
+		matrix.append(temp)
+
+	return  matrix
